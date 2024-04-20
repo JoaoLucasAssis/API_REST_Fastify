39da423046d569b622065bf9a96bc4e64101e01c
joaolucas <joaolucasdeassis.o@gmail.com>
2024-04-20 19:21:42 -0300

    chore: Verificação de config das rotas

--- src/hooks/onRoute.js
+++ src/hooks/onRoute.js
@@ -1,15 +1,17 @@
 /** @type{import('fastify').FastifyPluginAsync<>} */
+import { isAdmin, isAuthenticated } from './functions/index.js'
 
 export default async function onRouteHook(app, options) {
     app.addHook('onRoute', (routeOptions) => {
         // Inicialização
         routeOptions.onRequest = Array.isArray(routeOptions.onRequest) ? routeOptions.onRequest : [];
-        routeOptions.preHandler = Array.isArray(routeOptions.preHandler) ? routeOptions.preHandler : [];
 
         // onRequest
-        if (routeOptions.config?.requireAuthentication)
+        if (routeOptions.config?.requireAuthentication) {
             routeOptions.onRequest.push(isAuthenticated(app));
-        if (routeOptions.config?.requireAdmin)
+        }
+        if (routeOptions.config?.requireAdmin) {
             routeOptions.onRequest.push(isAdmin(app));
+        }
     });
 }
