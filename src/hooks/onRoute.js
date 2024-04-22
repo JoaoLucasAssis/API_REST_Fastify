168816cae56de15538910ece63d82fb5fd26c2f2
joaolucas <joaolucasdeassis.o@gmail.com>
2024-04-22 19:03:05 -0300

    fix: Removido linhas de console.log desnecessárias

--- src/hooks/onRoute.js
+++ src/hooks/onRoute.js
@@ -3,13 +3,11 @@ import { isAdmin, isAuthenticated } from './functions/index.js'
 
 export default async function onRouteHook(app, options) {
     app.addHook('onRoute', (routeOptions) => {
-        console.log('entrou no addHook')
         // Inicialização
         routeOptions.onRequest = Array.isArray(routeOptions.onRequest) ? routeOptions.onRequest : [];
 
         // onRequest
         if (routeOptions.config?.requireAuthentication) {
-            console.log('entrou no requireAuth')
             routeOptions.onRequest.push(isAuthenticated(app));
         }
         if (routeOptions.config?.requireAdmin) {
